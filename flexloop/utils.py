93809c02c43deba4c4fe1452879e18eb43a4c623
Michael Jendrusch <michael.jendrusch@embl.de>
2021-11-18 08:30:09 +0100

    add parse options.

---
+++ flexloop/utils.py
@@ -0,0 +1,27 @@
+import argparse
+
+class OptionType:
+  def __init__(self, default, parser=None, **kwargs):
+    self.default = default
+    self.type = parser or type(self.default)
+    self.kwargs = kwargs
+
+def parse_options(description, **kwargs):
+  parser = argparse.ArgumentParser(description=description)
+  for name in kwargs:
+    default = kwargs[name]
+    dtype = type(default)
+    argparse_kwargs = {}
+    if isinstance(default, OptionType):
+      dtype = default.type
+      argparse_kwargs = default.kwargs
+      default = default.default
+    elif isinstance(default, tuple):
+      default, dtype = default
+    parser.add_argument(
+      f"--{name}", default=default,
+      type=dtype, required=False,
+      **argparse_kwargs
+    )
+  return parser.parse_args()
+
