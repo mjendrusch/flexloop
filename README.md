4de7aa360a1c961a21eb8056ea9121797c131370
Michael Jendrusch <michael.jendrusch@embl.de>
2025-01-24 13:22:48 +0100

    update.

---
+++ README.md
@@ -0,0 +1,3 @@
+# flexloop
+Training loop utilities for training jax / haiku based models.
+Used for training [salad](https://github.com/mjendrusch/salad) models.
--- setup.py
+++ setup.py
@@ -7,12 +7,18 @@ setuptools.setup(
   name="flexloop",
   version="0.0.1",
   author="Michael Jendrusch",
-  author_email="jendrusch@stud.uni-heidelberg.de",
+  author_email="michael.jendrusch@embl.de",
   description="Minimalistic extensible training loop.",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/mjendrusch/flexloop/",
   packages=setuptools.find_packages(),
+  install_requires=[
+    "torch>=2.0",
+    "numpy",
+    "optax @ git+https://github.com/google-deepmind/optax.git",
+    "dm-haiku @ git+https://github.com/deepmind/dm-haiku"
+  ],
   classifiers=[
       "Programming Language :: Python :: 3",
       "License :: OSI Approved :: MIT License",
